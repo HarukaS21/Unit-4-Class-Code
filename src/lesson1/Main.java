e347718f5c25f5b8acc140843b02d778d5a52762
sakiyamh_wlhs <sakiyamh@wlhs.wlwv.k12.or.us>
2024-10-31 14:43:41 -0700

    n

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,5 +1,7 @@
 package lesson1;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         guessMyNumber();
@@ -7,6 +9,20 @@ public class Main {
     }
 
     public static void guessMyNumber(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("I'm thinking of a number between 1 and 10!");
+        System.out.println("See if you can guess my number!");
+        int secretNumber = (int)(Math.random()*9+1);
+        System.out.println("Enter your guess!");
+        tint userGuess = input.nextInt();
+        while (secretNumber =! userGuess) ;
+        {
+            System.out.println("Try again! Enter your guess!");
+            UserGuess = input.nextInt();
+        }
+        System.out.println("Correct!");
+
+        }
 
     }
 }
